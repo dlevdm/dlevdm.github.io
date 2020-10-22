d85088e9599c3b1d1db7890da13c438778198073
dlevdm <dlevdm@byui.edu>
2020-10-22 20:18:31 +0200

    Revert to Static

--- lesson6/js/webfontload.js
+++ lesson6/js/webfontload.js
@@ -1,5 +1,5 @@
   WebFont.load({
       google: {
-          families: ['Montserrat', 'Fredericka+the+Great', 'Ubuntu:700', 'Open+Sans']
+          families: ['Montserrat', 'Fredericka+the+Great', 'Ubuntu:700', 'Open+Sans:ital,400,700']
       }
   });
