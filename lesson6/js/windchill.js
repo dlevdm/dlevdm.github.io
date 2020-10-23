65fcb79222c857eb57ea251c4fa08e15b8b7de69
dlevdm <dlevdm@byui.edu>
2020-10-23 19:06:38 +0200

    Week 6

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,9 +1,11 @@
-const tempF = parseFloat(document.getElementById('temperatureinput').value);
-const speed = parseFloat(document.getElementById('windspeedinput').value);
-const wcf = 35.74 + 0.6215 * tempF - 35.75 * (speed ** 0.16) + 0.4275 * tempF * (speed ** 0.16);
+    let tempF = parseFloat(document.getElementById("temperatureinput").textContent);
+    let speed = parseFloat(document.getElementById("windspeedinput").textContent);
+    let output = "N/A";
+    if (tempF <= 50 && speed > 3) {
+        let windchillfactor = 35.74 + 0.6215 * tempF - 35.75 * Math.pow(speed, 0.16) + 0.4275 * tempF * Math.pow(speed, 0.16);
+        output = Math.round(windchillfactor) + "&#176;F";
+    }
+    document.getElementById("windchilloutput").innerHTML = output;
 
-if (tempF <= 50 && speed > 3) {
-    document.getElementById('windchilloutput').innerHTML = `${wcf} &#176;F`;
-} else {
-    document.getElementById('windchilloutput').innerHTML = 'N/A';
-}
+console.log(output); 
+
