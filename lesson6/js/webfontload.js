b996a97c48e407ed1000ec2182174838e85ddaf0
dlevdm <dlevdm@byui.edu>
2020-10-24 10:36:44 +0200

    Week 6 Update 1

--- lesson6/js/webfontload.js
+++ lesson6/js/webfontload.js
@@ -1,5 +1,5 @@
   WebFont.load({
       google: {
-          families: ['Montserrat', 'Fredericka+the+Great', 'Ubuntu:700', 'Open+Sans:ital,400,700']
+          families: ['Montserrat', 'Ubuntu:700', 'Open+Sans:ital,400,700']
       }
   });
