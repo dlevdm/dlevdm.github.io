4471c50d987da3c4c29dacd78d4407ca08b7f917
dlevdm <dlevdm@byui.edu>
2020-10-31 16:31:39 +0200

    Week 7 Updates 2

--- lesson7/js/lazyloadcode.js
+++ lesson7/js/lazyloadcode.js
@@ -1,30 +1,39 @@
-let imagesToLoad = document.querySelectorAll('img[data-src]');
+// Find all the images
+
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+
+// Add image load options
 
 const imgOptions = {
     threshold: 0,
-    rootMargin: "0 0 50px 0"
+    rootMargin: "0px 0px 20px 0px"
 };
+
+// Change image source from src to data-src OR change data-src to src 
 
 const loadImages = (image) => {
     image.setAttribute('src', image.getAttribute('data-src'));
-    image.onload = () => {
-        image.removeAttribute('data-src');
-    };
+    image.onload = () => {image.removeAttribute('data-src');};
 };
+
+// Load images
 
 if ('IntersectionObserver' in window) {
     const observer = new IntersectionObserver((items, observer) => {
         items.forEach((item) => {
-            if (item.isIntersecting) {
+    
+    if (item.isIntersecting) {
                 loadImages(item.target);
-                observer.unobserve(item.target);
-            }
+                observer.unobserve(item.target);}
         });
-    });
+    }, imgOptions);
+
     imagesToLoad.forEach((img) => {
         observer.observe(img);
     });
-} else {
+    } 
+    
+    else {
     imagesToLoad.forEach((img) => {
         loadImages(img);
     });
